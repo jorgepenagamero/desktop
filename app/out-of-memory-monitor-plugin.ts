2509114ff67fdf8e56276c1811ee27935d1cc109
Brendan Forster <github@brendanforster.com>
2018-09-05 23:08:24 -0300

    no need for this stuff

--- app/out-of-memory-monitor-plugin.ts
+++ app/out-of-memory-monitor-plugin.ts
@@ -7,10 +7,6 @@ OutOfMemoryMonitor.prototype.apply = function(compiler: webpack.Compiler) {
   compiler.hooks.beforeRun.tap(
     'before-run',
     (compilation: webpack.compilation.Compilation) => {
-      console.log(
-        `before-run fired for ${process.pid} - ${JSON.stringify(process.argv)}`
-      )
-
       require('node-oom-heapdump')({
         path: Path.resolve(__dirname, 'webpack'),
       })
